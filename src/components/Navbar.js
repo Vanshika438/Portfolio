c8ec24d5f0116b716d4d6da06fc13a2bb249b3df
Vanshika438 <vanshikasharma01012007@gmail.com>
2025-06-13 10:00:35 +0530

    Updated Css for Responsive Deesign

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import '../styles/Navbar.css';
 
-const Navbar=()=>{
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
         <p>Vanshika<span>Sharma</span></p>
       </div>
-      <div>
+      <div className="nav-links">
         <a href='#Home'>Home</a>
         <a href='#About'>About</a>
         <a href='#Resume'>Resume</a>
         <a href='#Projects'>Projects</a>
-        <a href='#Conatct'>Contact</a>
+        <a href='#Contact'>Contact</a>
       </div>
+
     </nav>
   );
 }
